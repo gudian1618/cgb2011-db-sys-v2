95b6c28dfc1b36a0892b64e35a548c579f77668c
gudian1618 <1761844211@qq.com>
2021-04-20 20:28:46 +0800

    new

---
+++ src/main/java/com/github/gudian1618/cgb2011dbsysv1/controller/PageController.java
@@ -0,0 +1,17 @@
+package com.github.gudian1618.cgb2011dbsysv1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author gudian1618
+ * @version v1.0
+ * @date 2021/4/20 8:25 下午
+ */
+
+@Controller
+public class PageController {
+
+
+
+}
