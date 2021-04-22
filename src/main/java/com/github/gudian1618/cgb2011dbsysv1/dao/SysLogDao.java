017f5c6709108e73e60c6962a0d0575a8e11e7ef
gudian1618 <1761844211@qq.com>
2021-04-22 21:29:44 +0800

    new

---
+++ src/main/java/com/github/gudian1618/cgb2011dbsysv1/dao/SysLogDao.java
@@ -0,0 +1,34 @@
+package com.github.gudian1618.cgb2011dbsysv1.dao;
+
+import com.github.gudian1618.cgb2011dbsysv1.entity.SysLog;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+/**
+ * @author gudian1618
+ * @version v1.0
+ * @date 2021/4/21 9:23 下午
+ */
+
+@Mapper
+public interface SysLogDao {
+
+    /**
+     * 基于查询条件统计用户行为的日志记录
+     * @param username
+     * @return
+     */
+    int getRowCount(String username);
+
+    /**
+     * 查询当前页要呈现的用户行为日志
+     * @param username
+     * @param startIndex 当前页的起始位置
+     * @param pageSize 当前页的页面大小(每页最多显示多少条)
+     * @return
+     */
+    List<SysLog> findPageObjects(@Param("username") String username, Long startIndex, Integer pageSize);
+
+}
---
+++ src/test/java/com/github/gudian1618/cgb2011dbsysv1/dao/SysLogDaoTests.java
@@ -0,0 +1,36 @@
+package com.github.gudian1618.cgb2011dbsysv1.dao;
+
+import com.github.gudian1618.cgb2011dbsysv1.entity.SysLog;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+/**
+ * @author gudian1618
+ * @version v1.0
+ * @date 2021/4/21 9:44 下午
+ */
+
+@SpringBootTest
+public class SysLogDaoTests {
+
+    @Autowired
+    private SysLogDao sysLogDao;
+
+    @Test
+    public void testGetRowCount() {
+        int rowCount = sysLogDao.getRowCount(null);
+        System.out.println("rowCount=" + rowCount);
+    }
+
+    @Test
+    public void testFindPageObjects() {
+        List<SysLog> list = sysLogDao.findPageObjects("admin", 0L, 5);
+        for (SysLog log : list) {
+            System.out.println(log);
+        }
+    }
+
+}
